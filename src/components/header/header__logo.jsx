b54ccf93f97fd0b59bfada080dbc85cee9933fa8
REDFOX39 <daniil.fox1998@yandex.ru>
2021-10-26 20:37:32 +0300

    Dev 0.2 > Рефакторинг

--- src/components/header/header__logo.jsx
+++ src/components/header/header__logo.jsx
@@ -6,7 +6,7 @@ class HeaderLogo extends React.Component {
 			<div className="header__logo-wrapper logo" >
 				<picture className="logo__picture">
 					<source srcSet="img/logo/header-logo--desktop.png"/>
-					<img className="logo__img" src="img/logo/header-logo--mobile.png" alt="Logotype"/>
+					<img className="logo__img" src={'%PUBLIC_URL%/img/logo/header-logo--mobile.png'} alt="Logotype"/>
 				</picture>
 			</div>
 		)
--- src/components/index/main/privileges/pr-block-4.jsx
+++ src/components/index/main/privileges/pr-block-4.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 
+const Components = {
+	ArticleType1: function(props) {
+		return(
+			<article className={props.artcClass}>
+				<h2 className={props.hClass}>{props.title}</h2>
+				<span className={props.spClass}>{props.decoEl}</span>
+				<p className={props.pClass}>{props.content}</p>
+			</article>
+		)
+	}
+}
+
 class PrBlock4 extends React.Component {
 	render() {
 		return(
 			<div className="privileges__block privileges__block-4">
-				<article>
-					<h2>Небольшая площадь</h2>
-					<span>— №3 —</span>
-					<p>Все интересные места находятся очень близко</p>
-				</article>
-				<article>
-					<h2>Красивая дорога</h2>
-					<span>— №4 —</span>
-					<p>Ехать в Седону из Лас-Вегаса совсем не скучно!</p>
-				</article>
-				<article>
-					<h2>Мало туристов</h2>
-					<span>— №5 —</span>
-					<p>Большинство едет в гранд каньон и толпится там</p>
-				</article>
+			<Components.ArticleType1 title="Небольшая площадь" decoEl="— №3 —" content="Все интересные места находятся очень близко"/>
+			<Components.ArticleType1 title="Красивая дорога" decoEl="— №4 —" content="Ехать в Седону из Лас-Вегаса совсем не скучно!"/>
+			<Components.ArticleType1 title="Мало туристов" decoEl="— №5 —" content="Большинство едет в гранд каньон и толпится там"/>
 			</div>
 		)
 	}

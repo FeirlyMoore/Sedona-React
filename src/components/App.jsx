6f2a2bf66e106023698a47d977000c3768c5ff13
REDFOX39 <daniil.fox1998@yandex.ru>
2021-10-29 20:45:50 +0300

    Dev 0.4 > Index markup completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './header/header.jsx';
 import Intro from './index/intro.jsx';
 import Main from './index/main/main.jsx';
+import Footer from './footer/footer.jsx';
 
 class App extends React.Component{
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component{
         <Header/>
         <Intro/>
         <Main/>
+        <Footer/>
       </div>
     )
   }

ca436459c8965df1b8d8eeccfcba525109c1f4ee
REDFOX39 <daniil.fox1998@yandex.ru>
2021-12-06 23:54:52 +0300

    Dev 0.5.9 > Form markup p.4

--- src/components/form/form-main.jsx
+++ src/components/form/form-main.jsx
@@ -12,15 +12,15 @@ class FormMain extends React.Component {
 						<h2 className="form-article__title">Представьтесь :</h2>
 						<label className="form__label">
 							Имя* :
-							<input className="form__input" type="text" placeholder="Акакий" name="name" required/>
+							<input className="form__input form__input--text" type="text" placeholder="Акакий" name="name" required/>
 						</label>
 						<label className="form__label">
 							Фамилия* :
-							<input className="form__input" type="text" placeholder="Нюхнина" name="surname" required/>
+							<input className="form__input  form__input--text" type="text" placeholder="Нюхнина" name="surname" required/>
 						</label>
 						<label className="form__label">
 							Отчество* :
-							<input className="form__input" type="text" placeholder="Евпатьевич" name="patronymic" required/>
+							<input className="form__input  form__input--text" type="text" placeholder="Евпатьевич" name="patronymic" required/>
 						</label>
 					</article>
 					<article className="form__article">

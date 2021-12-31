a79b99eb4b701cb1b88920d67cd08a8c76532bb4
REDFOX39 <daniil.fox1998@yandex.ru>
2022-01-01 01:44:51 +0300

    Dev 0.6.1 > Intro mobile stylization : Part 0.3

--- src/components/index/intro.jsx
+++ src/components/index/intro.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Intro extends React.Component {
 	render() {
 		return(
-			<div className="intro">
+			<div className="intro" style={{backgroundImage: 'url(./img/decorative/background--mobile.png)', backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
+
 				<div className="intro__title-block title-block-1">
 					<img className="intro__img" src="img/logo/intro.svg" alt="Intro"/>
 				</div>
